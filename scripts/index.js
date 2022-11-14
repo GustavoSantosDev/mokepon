7f2f24398f7deacc612edb2667d0b012fba1bb0e
GustavoSantosDev <jgustavo.santosc@gmail.com>
2022-11-14 08:06:43 -0700

    Funcionalidad de seleccion de mascotas

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,60 @@
+let ataqueJugador
+
+
+
+
 function iniciarJuego(params) {
     let botonMascotaJugador = document.getElementById('boton-mascota')
-    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)  
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonFuego.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonFuego.addEventListener('click', ataqueTierra)
 }
 
 function seleccionarMascotaJugador() {
-    if (document.getElementById('hipodogue').checked == true) {
+    let inputDoge = document.getElementById('hipodogue')
+    let inputPepo = document.getElementById('capipepo')
+    let inputGueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-Jugador')
+
+
+    if (inputDoge.checked == true) {
         alert("Seccionaste a HipoDoge")
-    }else if (document.getElementById('capipepo').checked == true) {
+        spanMascotaJugador.innerHTML = "HipoDoge"
+    }else if (inputPepo.checked == true) {
         alert("Seccionaste a CapiPepo")
-    }else if (document.getElementById('ratigueya').checked == true) {
+        spanMascotaJugador.innerHTML = "CapiPepo"
+    }else if (inputGueya.checked == true) {
         alert("Seccionaste a RatiGueya")
+        spanMascotaJugador.innerHTML = "RatiGueya"
+    }else(
+        alert("No has seleccionado todavia ninguna Mascota")
+    )
+
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+    let seleccionAleatorio = aleatorio(1,3)
+    if (seleccionAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = "Hipodogue"
+    }else if (seleccionAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = "CapiPepo"
+    }else {
+        spanMascotaEnemigo.innerHtml = "RatiGueya"
     }
 }
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+
 
 
 
